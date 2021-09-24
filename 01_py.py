8eca21170bd5384cf591cf9fbee78fd83cc58e98
dib1000 <dib5017@gmail.com>
2021-09-24 08:38:32 -0400

    renamed file

---
+++ 01_py.py
@@ -0,0 +1,16 @@
+#Question: What do you mean by "print a name"
+
+import random
+pd1 = ["emma","shriya","william","aaron"]
+pd2 = ["andrew", "yuqing","michael"]
+
+def printName():
+    period = input('Pick a period (Input 1 or 2): ')
+    if period == '1':
+        print(pd1[random.randint(0,len(pd1)-1)])
+    elif period == '2':
+        print(pd2[random.randint(0,len(pd2)-1)])
+    else:
+        print("This input goes against the rules")
+        printName()
+    
