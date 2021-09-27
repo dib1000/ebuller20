7e9ba25f943c8dd15f9daae6684c61beda172cd0
dib1000 <dib5017@gmail.com>
2021-09-27 08:27:33 -0400

    Changed lists to dictionaries

--- 01_py.py
+++ 01_py.py
@@ -1,18 +1,22 @@
 #Emma Buller
 #SoftDev
-#K05 -- Refactoring
-# 2021--9--26
+#K05 -- Better Living Through Amalgamation
+# 2021-9-27
 
 import random
 pd1 = ["emma","shriya","william","aaron"]
 pd2 = ["andrew", "yuqing","michael"]
+krewes = {
+    "one" : pd1,
+    "two" : pd2
+    }
 
 def printName():
     period = input('Pick a period (Input 1 or 2): ')
     if period == '1':
-        print(pd1[random.randint(0,len(pd1)-1)])
+        print(krewes["one"][random.randint(0,len(pd1)-1)])
     elif period == '2':
-        print(pd2[random.randint(0,len(pd2)-1)])
+        print(krewes["two"][random.randint(0,len(pd2)-1)])
     else:
         print("This input goes against the rules")
         printName()
