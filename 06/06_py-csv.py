3dc64c1542eb67fff89172387790b52928fb2d6b
Emma Buller <dib5017@gmail.com>
2021-09-28 19:59:25 -0400

    Updated heading, started the process of splitting the list items to put them into dictionaries

--- 06/06_py-csv.py
+++ 06/06_py-csv.py
@@ -1,9 +1,18 @@
-#Emma Buller
+#Cashew (Shriya Anand, Emma Buller, William Chen)
 #SoftDev
 #K06 -- Stl/O: Divine your Destiny! (Parsing through files)
 #2021-09-29
 
+#reading file and splitting everything
 file = open("occupations.csv","r")
 contents = file.read()
 contentlist = contents.split("\n")
-print(contentlist)
+
+
+weight = 0
+occupations = {}
+for x in range(1,len(contentlist)-1):
+    item = contentlist[x].split("\t")
+    print(item[0])
+    print(item[1])
+
