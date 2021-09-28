67f3ff38929efb08f4944c3888f54d77e613ee35
dib1000 <dib5017@gmail.com>
2021-09-28 08:40:53 -0400

    File is now being read

--- 06/06_py-csv.py
+++ 06/06_py-csv.py
@@ -2,3 +2,8 @@
 #SoftDev
 #K06 -- Stl/O: Divine your Destiny! (Parsing through files)
 #2021-09-29
+
+file = open("occupations.csv","r")
+contents = file.read()
+contentlist = contents.split("\n")
+print(contentlist)
