9c5ed3bb9f019dd3627c393eb105dd91fcee2c06
emmabu11er <59813263+dib1000@users.noreply.github.com>
2021-09-27 20:02:24 -0400

    Restored old version of 01_py

--- 01_py.py
+++ 01_py.py
@@ -1,29 +1,18 @@
 #Emma Buller
 #SoftDev
 #K05 -- Better Living Through Amalgamation
-# 2021-9-27
-
-#DISCOVERIES:
-#We remembered that dictionaries exist
-#it's elif instead of else if
+# 2021--9--26
 
 import random
-pd1 = ["emma","shriya","william","aaron","owen","haotian","ishraq","ivan l",
-       "michelle", "naomi", "lewis cass", "renggeng", "oscar", "tami", "aryman",
-       "alejandro","deven","tina","yusuf","ella","ltw", "julia","angela"
-       ,"christopher","sadid","gavin","ivan m"]
+pd1 = ["emma","shriya","william","aaron"]
 pd2 = ["andrew", "yuqing","michael"]
-krewes = {
-    "one" : pd1,
-    "two" : pd2
-    }
 
 def printName():
     period = input('Pick a period (Input 1 or 2): ')
     if period == '1':
-        print(krewes["one"][random.randint(0,len(pd1)-1)])
+        print(pd1[random.randint(0,len(pd1)-1)])
     elif period == '2':
-        print(krewes["two"][random.randint(0,len(pd2)-1)])
+        print(pd2[random.randint(0,len(pd2)-1)])
     else:
         print("This input goes against the rules")
         printName()
