81f2ac80a1971bb54e57c3efc7cd576e3e697208
dib1000 <dib5017@gmail.com>
2021-09-29 08:32:23 -0400

    Edited code so that it used csv reading

--- 06/06_py-csv.py
+++ 06/06_py-csv.py
@@ -10,23 +10,17 @@
 # random.choices() method. We decided to use that and used some dictionary methods to
 # split the keys and values into lists that can be inputted to the choices method.
 
+import csv
 import random
-
 def selectJob():
-    #reading file and getting rid of the new lines
-    file = open("occupations.csv","r")
-    contents = file.read()
-    contentlist = contents.split("\n")
-    
-    #putting the content of the file into dictionaries
-    occupations = {}
-    for x in range(1,len(contentlist)-2): #no top line or last 2 lines that don't have the same structure as the other lines of file
-        item = contentlist[x].split(" \t") #the job names and percentage were connected by this sequence of characters
-        occupations[item[0]] = float(item[1])
+    dictionary ={}
+    with open('o.csv', newline='') as csvfile:
+         reader = csv.DictReader(csvfile)
+         for row in reader:
+             if (row['Job Class']!= 'Total' and row['Job Class']!= 'Job Class'):
+                 dictionary[row['Job Class']] = float(row['Percentage'])
+    job = random.choices(list(dictionary.keys()),list(dictionary.values()), k = 1)
+    return job[0]
 
-    jobs = list(occupations.keys()) #list for the jobs
-    percentages = list(occupations.values()) #list for the percentages
-    job = random.choices(jobs, percentages, k = 1); #method chooses 1 of the jobs randomly based on weighted
-                                                    #average based on percentages list. It makes a 1 item list
-    return job[0] #only returns the job, not the list
-    
+
+
