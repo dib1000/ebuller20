f1b3458aa6b6c60a073836af46db302c57647ed3
Emma Buller <dib5017@gmail.com>
2021-09-28 20:23:10 -0400

    put code into function

--- 06/06_py-csv.py
+++ 06/06_py-csv.py
@@ -3,15 +3,15 @@
 #K06 -- Stl/O: Divine your Destiny! (Parsing through files)
 #2021-09-29
 
-#reading file and splitting everything
-file = open("occupations.csv","r")
-contents = file.read()
-contentlist = contents.split("\n")
-
-
-occupations = {}
-for x in range(1,len(contentlist)-2):
-    item = contentlist[x].split(" \t")
-    occupations[item[0]] = float(item[1])
-
-print(occupations)
+def selectJob():
+    #reading file and getting rid of the new lines
+    file = open("occupations.csv","r")
+    contents = file.read()
+    contentlist = contents.split("\n")
+    
+    #putting the content of the file into dictionaries
+    occupations = {}
+    for x in range(1,len(contentlist)-2): #no top line or last 2 lines that don't have the same structure as the other lines of file
+        item = contentlist[x].split(" \t") #the job names and percentage were connected by this sequence of characters
+        occupations[item[0]] = float(item[1])
+    print(occupations)
