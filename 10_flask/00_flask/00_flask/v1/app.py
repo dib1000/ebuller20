b7cfdb9347a61f9db09f1ce6721014e8dfa1c12c
dib1000 <dib5017@gmail.com>
2021-10-04 08:24:03 -0400

    Added the folders for assignment

---
+++ 10_flask/00_flask/00_flask/v1/app.py
@@ -0,0 +1,13 @@
+# Cashew (Shriya Anand, Emma Buller, William Chen)
+# SoftDev
+#K10-Putting Little Pieces Together
+#2021-10-04
+
+from flask import Flask
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")       #assign fxn to route
+def hello_world():
+    return "No hablo queso!"
+
+app.run()
---
+++ 10_flask/00_flask/00_flask/v2/app.py
@@ -0,0 +1,15 @@
+# Cashew (Shriya Anand, Emma Buller, William Chen)
+# SoftDev
+#K10-Putting Little Pieces Together
+#2021-10-04
+
+from flask import Flask
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")       #assign fxn to route
+def hello_world():
+    print("about to print __name__...")
+    print(__name__)   #where will this go?
+    return "No hablo queso!"
+
+app.run()
