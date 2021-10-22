0906198b19ef857c71150e02bed26f52966026e1
dib1000 <dib5017@gmail.com>
2021-10-22 08:39:27 -0400

    Added comments

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -7,26 +7,26 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 
-DB_FILE="discobandit.db"
+DB_FILE="discobandit.db" #This is the file you do sqlite commands on. Remove it before you run the file again
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 #==========================================================
-c.execute("CREATE TABLE roster (name TEXT, age INTEGER, id INTEGER)")
+c.execute("CREATE TABLE roster (name TEXT, age INTEGER, id INTEGER)") #NEW TABLE
 
-with open('students.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter= ",")
+with open('students.csv') as csvfile: #open file
+    reader = csv.reader(csvfile, delimiter= ",") #splits line for text
     for row in reader:
-        name = row[0]
+        name = row[0] #row is split into a list of strings
         age = row[1]
         id = row[2]
-        if(age != "age"):
-            command = "INSERT INTO roster VALUES ('" + name + "'," + age + "," + id + ")"
-            c.execute(command);
+        if(age != "age"): #skips first line
+            command = "INSERT INTO roster VALUES ('" + name + "'," + age + "," + id + ")" #quotes around name = TEXT/strng; no quotes = INTEGER
+            c.execute(command); #rows inserted into table
 
-c.execute("CREATE TABLE classes (code TEXT, mark INTEGER, id INTEGER)")
-with open('courses.csv') as file:
+c.execute("CREATE TABLE classes (code TEXT, mark INTEGER, id INTEGER)") #NEW TABLE
+with open('courses.csv') as file: #same as first table, but with different variable names
     r = csv.reader(file, delimiter= ",")
     for row in r:
         code = row[0]
