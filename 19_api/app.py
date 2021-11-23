4103d824c96b4c0c5f919ef898d5b304dc1ce6f5
dib1000 <dib5017@gmail.com>
2021-11-23 08:38:20 -0500

    Made files

---
+++ 19_api/app.py
@@ -0,0 +1,18 @@
+# Lightning Cheese (Emma Buller and Deven Maheshwari)
+# SoftDev
+# K19 -- A RESTful Journey (Using RESTapis)
+# 2021-11-23
+# time spent:
+
+from flask import Flask, render_template  #Q0: What happens if you remove render_template from this line?
+import urllib.request
+app = Flask(__name__)
+
+@app.route("/")
+def REST():
+    d = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=uhAKVvq7ohfNOGPx5IMu3bUGcAsaikXHR7IYMtjw")
+    return render_template("main.html", img=d.url)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
