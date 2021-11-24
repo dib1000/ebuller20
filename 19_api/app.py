131dab45912d16f60557ce04f92735a3f4493522
Emma Buller <dib5017@gmail.com>
2021-11-23 22:04:44 -0500

    Added image successfully

--- 19_api/app.py
+++ 19_api/app.py
@@ -2,17 +2,19 @@
 # SoftDev
 # K19 -- A RESTful Journey (Using RESTapis)
 # 2021-11-23
-# time spent:
+# time spent: 45 Minutes
 
 from flask import Flask, render_template  #Q0: What happens if you remove render_template from this line?
 import urllib.request
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def REST():
     d = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=uhAKVvq7ohfNOGPx5IMu3bUGcAsaikXHR7IYMtjw")
-    x = json.loads(d)
-    return render_template("main.html", img=x[url])
+    x = d.read()
+    y = json.loads(x)
+    return render_template("main.html", img=y["url"])
 
 if __name__ == "__main__":
     app.debug = True
